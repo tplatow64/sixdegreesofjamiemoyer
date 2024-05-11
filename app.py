0c120fb13eb5e29b43ea5eccda1c8e912b9c8a0d
tplatow64 <typlatow@gmail.com>
2024-05-11 15:07:40 -0400

    oops i did it again

--- app.py
+++ app.py
@@ -9,8 +9,11 @@ app = Flask(__name__)
 env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
 app.config.from_object(env_config)
 uri = app.config.get('NEO_URI')
+pwd = app.config.get('NEO_PWD')
 print('connecting')
 driver = GraphDatabase.driver(uri, auth=("neo4j", pwd))
+print('connected')
+
 
 @app.route("/")
 def index():
