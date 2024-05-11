15c86d388a7aa17af19d882be3d41e437e1bdc05
tplatow64 <typlatow@gmail.com>
2024-05-11 13:06:12 -0400

    trying to get env variable in test app

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
+env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
+app.config.from_object(env_config)
+uri = app.config.get('NEO_URI')
+
 @app.route("/")
 def index():
-    return "Hello World!"
+    return f"URI is {uri}"
