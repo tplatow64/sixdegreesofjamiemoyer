ca490e4164ff8d6489239ed0ccf2f9fde45fa47d
tplatow64 <typlatow@gmail.com>
2024-05-11 15:15:09 -0400

    add session var

--- app.py
+++ app.py
@@ -13,20 +13,22 @@ pwd = app.config.get('NEO_PWD')
 print('connecting')
 driver = GraphDatabase.driver(uri, auth=("neo4j", pwd))
 print('connected')
+with driver.session(database="neo4j") as session:
+    print('session defined')
 
-def find_person(id: str) -> json:
-    print(f'finding {id}')
-    query = f"""MATCH (p:Person)
-        WHERE p.debut <> "" AND p.playerID = $p1
-        RETURN p.playerID as playerID, p.name + ' ' + left(p.debut,4) + ' - ' + left(p.finalGame, 4) AS name
-        ORDER BY p.nameLast      
-        """
-    ret_val = session.run(query, p1=id)
-    print('query done')
-    return ret_val.data()[0]['name']
+    def find_person(id: str) -> json:
+        print(f'finding {id}')
+        query = f"""MATCH (p:Person)
+            WHERE p.debut <> "" AND p.playerID = $p1
+            RETURN p.playerID as playerID, p.name + ' ' + left(p.debut,4) + ' - ' + left(p.finalGame, 4) AS name
+            ORDER BY p.nameLast      
+            """
+        ret_val = session.run(query, p1=id)
+        print('query done')
+        return ret_val.data()[0]['name']
 
 
-@app.route("/")
-def index():
-    value = find_person('rojasjo03')
-    return f"Hello World! {value}"
+    @app.route("/")
+    def index():
+        value = find_person('rojasjo03')
+        return f"Hello World! {value}"
